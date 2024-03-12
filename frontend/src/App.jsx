f8d6018ea02cbf291f2b1fb59ea4288dab6be3f2
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-03-12 16:00:58 +0300

    created component layout and privacy footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
-import Header from './components/Header'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import Policy from './components/policy/Policy'
 
 function App() {
   return (
     <>
     <Header />
+    <Policy />
+    <Footer />
     </>
   )
 }
