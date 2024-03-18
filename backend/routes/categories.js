707cfb043ded6d5098a6bd3a35a8fd091d8500b1
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-03-18 17:00:37 +0300

    created new category and added post and get requests

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const categories = require("../models/Category");
+const Category = require("../models/Category");
 
-// get all categories
+router.post("/", async (req, res) => {
+  try {
+    const { name, img } = req.body;
+    const newCategory = new Category({
+      name,
+      img,
+    });
+
+    await newCategory.save();
+
+    res.status(201).json(newCategory);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 router.get("/", async (req, res) => {
   res.send("Get categories");
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 const app = express();
 const mainRoute = require("./routes/index.js");
 const port = 5000;
@@ -9,20 +9,19 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGO_URI,
-      {}
-    );
+    await mongoose.connect(process.env.MONGO_URI, {});
     console.log(`MongoDB connected`);
   } catch (error) {
     throw error;
   }
 };
 
+// middlewares
+app.use(express.json());
 
 app.use("/api", mainRoute);
 
 app.listen(port, () => {
-    connectDB();
+  connectDB();
   console.log(`Server is running on port ${port}`);
 });
