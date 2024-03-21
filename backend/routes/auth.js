2e0e33d99e345ae626a494306a2dc038ad46be15
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-03-21 13:18:42 +0300

    added bcryscript library for protected user password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,22 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ error: "Email address is already registed." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const newUser = await new User({
       username,
       email,
       password,
+      password: hashedPassword,
     });
+
     await newUser.save();
-    res.status(200).json(newUser);
-    console.log(req.body);
+    res.status(201).json(newUser);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ error: "Server error." });
   }
 });
-
 module.exports = router;
