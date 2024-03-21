404c332eb45b151bcd83da64d53d70d1f2cf12e6
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-03-21 14:10:34 +0300

    added login function

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -14,9 +14,9 @@ router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
     const defaultAvatar = generateRandomAvatar();
-    const existingUser = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-    if (existingUser) {
+    if (user) {
       return res
         .status(400)
         .json({ error: "Email address is already registed." });
@@ -36,4 +36,32 @@ router.post("/register", async (req, res) => {
     res.status(500).json({ error: "Server error." });
   }
 });
+
+// login the user (Login)
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: "Invalid email" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(404).json({ error: "Invalid password." });
+    }
+
+    res.status(200).json({
+      id: user._id,
+      email: user.email,
+      username: user.username,
+      avatar: user.avatar,
+      role: user.role,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
 module.exports = router;
