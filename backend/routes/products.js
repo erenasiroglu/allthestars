18557d225c4c25e26a2a47c1b9dfc2bd0f937ed6
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-03-21 14:36:14 +0300

    added new product process

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const Product = require("../models/Product");
 
 // get all products
 
 router.get("/", async (req, res) => {
   res.send("Get products");
+});
+
+// create new product
+router.post("/", async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.status(201).json(newProduct);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
