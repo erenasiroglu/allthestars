fe56c231b20771794988ef683eab39a1c757402e
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-03-28 16:57:04 +0300

    added home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,12 @@
-import Campaigns from "./components/Campaigns/Campaign";
-import Categories from "./components/Categories/Categories";
-import Footer from './components/Layout/Footer/Footer'
-import Header from './components/Layout/Header/Header'
-import Policy from './components/Layout/Policy/Policy'
-import Products from './components/Layout/Products/Products';
-import Sliders from "./components/Slider/Sliders";
-import Blogs from "./components/Blogs/Blog";
-import Brands from "./components/Brands/Brands";
-import CampaignSingle from "./components/CampaignSingle/CampaignSingle";
-import './App.css'
+import HomePage from "./pages/HomePage";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <Header />
-    <Sliders/>
-    <Categories />
-    <Campaigns />
-    <Products />
-    <Blogs />
-    <Brands />
-    <CampaignSingle />
-    <Policy />
-    <Footer />
+      <HomePage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/ShopPage.jsx
@@ -0,0 +1,9 @@
+const ShopPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShopPage
