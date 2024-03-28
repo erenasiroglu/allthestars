422d796af1707ffb17bbb54775cefd71dff58d8f
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-03-28 17:03:04 +0300

    added vercel speed insights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,11 @@ import './index.css'
 // import MainLayout from './layouts/MainLayout.jsx'
 // import AdminLayout from './layouts/AdminLayout.jsx'
 import App from './App.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <App/>
+   <SpeedInsights />
   </React.StrictMode>,
 )
