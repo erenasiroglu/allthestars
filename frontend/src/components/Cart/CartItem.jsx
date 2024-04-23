aa2654edd51c4fad9099f64677d8520576514436
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-04-24 01:04:18 +0300

    added to cart opearations

--- frontend/src/components/Cart/CartItem.jsx
+++ frontend/src/components/Cart/CartItem.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import PropTypes from "prop-types";
 import { CartContext } from "../../context/CartProvider";
 
-export const CartItem = ({ cartItem }) => {
+export const CartItem = (cartItem) => {
   const { removeFromCart } = useContext(CartContext);
   return (
     <tr className="cart-item">
@@ -15,9 +15,11 @@ export const CartItem = ({ cartItem }) => {
         ></i>
       </td>
       <td>{cartItem.name}</td>
-      <td>$108.00</td>
-      <td className="product-quantity">1</td>
-      <td className="product-subtotal">$108.00</td>
+      <td>${cartItem.price.newPrice.toFixed(2)}</td>
+      <td className="product-quantity">{cartItem.quantity}</td>
+      <td className="product-subtotal">
+        ${(cartItem.price.newPrice * cartItem.quantity).toFixed(2)}
+      </td>
     </tr>
   );
 };
--- frontend/src/components/Cart/CartTotals.jsx
+++ frontend/src/components/Cart/CartTotals.jsx
@@ -1,13 +1,36 @@
-export const CartTotals = () => {
+
+import { useContext, useState } from "react";
+import { CartContext } from "../../context/CartProvider";
+
+const CartTotals = () => {
+  const [fastCargoChecked, setFastCargoChecked] = useState(false);
+  const { cartItems } = useContext(CartContext);
+
+  const cartItemTotals = cartItems.map((item) => {
+    const itemTotal = item.price.newPrice * item.quantity;
+
+    return itemTotal;
+  });
+
+  const subTotals = cartItemTotals.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue;
+  }, 0);
+
+  const cargoFee = 15;
+
+  const cartTotals = fastCargoChecked
+    ? (subTotals + cargoFee).toFixed(2)
+    : subTotals.toFixed(2);
+
     return (
       <div className="cart-totals">
-        <h2>cart totals</h2>
+        <h2>Cart totals</h2>
         <table>
           <tbody>
             <tr className="cart-subtotal">
               <th>Subtotal</th>
               <td>
-                <span id="subtotal">$316.00</span>
+                <span id="subtotal">${subTotals.toFixed(2)}</span>
               </td>
             </tr>
             <tr>
@@ -17,7 +40,12 @@ export const CartTotals = () => {
                   <li>
                     <label>
                       Fast Cargo: $15.00
-                      <input type="checkbox" id="fast-cargo" />
+                      <input
+                      type="checkbox"
+                      id="fast-cargo"
+                      checked={fastCargoChecked}
+                      onChange={() => setFastCargoChecked(!fastCargoChecked)}
+                    />
                     </label>
                   </li>
                   <li>
@@ -29,7 +57,7 @@ export const CartTotals = () => {
             <tr>
               <th>Total</th>
               <td>
-                <strong id="cart-total">$316.00</strong>
+                <strong id="cart-total">${cartTotals}</strong>
               </td>
             </tr>
           </tbody>
